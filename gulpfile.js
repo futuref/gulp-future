b18426334baf01de7702656a3a91a27525dcf0e7
mac <mac@future.local>
2017-06-06 20:26:33 +0800

    新增检查js,检查scss，和编译scss

---
+++ gulpfile.js
@@ -0,0 +1,71 @@
+var gulp = require('gulp'),
+    config = require('./config.json'),
+    autoprefixer =require('autoprefixer'),
+    cssnano = require('cssnano'),
+    browserSync = require('browser-sync'),
+    runSequence = require('run-sequence'),
+    gulpLoadPlugins = require('gulp-load-plugins'),
+    plugins = gulpLoadPlugins();
+
+function isFixed(file) {
+  return file.eslint != null && file.eslint.fixed;
+}
+
+//自动刷新
+gulp.task('browserSync',function(){
+  browserSync({
+    server:{
+      baseDir:[config.path.root],
+      directory:true
+    },
+    // 监听以下文件的变化，如果变化就会自动刷新浏览器
+    files:[
+      config.path.srclintScss,
+      config.path.srclintScript,
+      config.path.srcHtml
+    ]
+  })
+})
+
+// 检查js
+gulp.task('js-lint',function() {
+  return gulp.src(config.path.srclintScript)
+        .pipe(plugins.cached(config.path.srclintScript))
+        .pipe(plugins.eslint({fix:true}))
+        .pipe(plugins.eslint.format())
+        .pipe(plugins.if(isFixed, gulp.dest(config.path.distScript)))
+})
+
+// 检查 scss
+gulp.task('scss-lint',function() {
+  return gulp.src([config.path.srclintScss,'!' + config.path.srcExcludeScss])
+         .pipe(plugins.cached(config.path.srclintScss))
+         .pipe(plugins.scssLint())
+})
+
+// 编译scss
+gulp.task('css',function() {
+  var processors = [
+    autoprefixer(config.autoprefixer),
+    cssnano
+  ]
+  return gulp.src(config.path.srcComplieScss)
+         .pipe(plugins.sass().on('error',plugins.sass.logError))
+         .pipe(plugins.plumber())
+         .pipe(plugins.postcss(processors))
+         .pipe(gulp.dest(config.path.srcCss))
+         .pipe(gulp.dest(config.path.distCss))
+         .pipe(plugins.notify('scss编译完成'))
+})
+
+// 监听任务
+gulp.task('watch',function() {
+  gulp.watch(config.path.srclintScss,['css','scss-lint']);
+  gulp.watch(config.path.srclintScript,['js-lint']);
+  gulp.watch(config.path.srcHtml);
+})
+
+// 默认开发任务
+gulp.task('default',function(callback){
+  runSequence(['js-lint','scss-lint','css','browserSync','watch'],callback);
+})
