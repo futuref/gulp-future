0efc3e05f4d5ceafb1d77825e347244590a8ca5f
mac <mac@future.local>
2017-06-06 20:30:11 +0800

    添加eslint规则,scsslint规则,测试文件

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  'extends': "google",
+  // 执行环境
+  'env': {
+    "browser":true,
+    "node":true
+  },
+  // 根据需要修改一些规则
+  'rules': {
+    'max-len': [1,
+      {
+        'code': 100,
+        'tabWidth': 2,
+        'ignoreUrls': true,
+        "ignoreComments": true, // 忽略所有拖尾注释和行内注释,
+				ignorePattern: '^\s*(app)?.(controller|directive|service|factory|provider)'
+			}
+    ], // warn 100强制一行的最大长度
+     'no-var': 0  //禁用var，用let和const代替var
+  }
+}
---
+++ src/script/app.js
@@ -0,0 +1,4 @@
+var arr = [1, 2, 3];
+console.log(arr);
+var gg="haha";
+console.log(gg);
